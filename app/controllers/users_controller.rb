b3776dc39c8ef8734dfaeb3a839b1c3ec1e9bad0
TomokazuShimada <pikkoro.dreamers.high.tmkz@gmail.com>
2019-10-29 13:17:01 +0000

    勤怠Bホントに終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info,]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info,]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info, :index, :show]
   before_action :set_one_month, only: :show
-
+  
   def index
     @users = User.paginate(page: params[:page]).search(params[:search])
   end
   
-  def search
-    @users = User.search(page: params[:page]).(params[:search])
-  end
-
   def show
     @worked_sum = @attendances.where.not(started_at: nil).count
   end
@@ -60,6 +56,9 @@ class UsersController < ApplicationController
       flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
     end
     redirect_to users_url
+  end
+  
+  def edit_basic_time
   end
 
   private
